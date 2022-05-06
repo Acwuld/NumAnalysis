1429135c75376910561f16543463982f0a61d05c
Leo <104699857+mathliker@users.noreply.github.com>
2022-05-06 11:58:25 +0800

    Update Lagrange_1.py

--- 插值与拟合/Lagrange_1.py
+++ 插值与拟合/Lagrange_1.py
@@ -1,3 +1,12 @@
-import numpy
-def Lagrange_1(x,y,a):
-  
+def Larange_1(A,a):
+    A=np.array(A,dtype=float)
+    s,n=0.0,A.shape[0]
+    for i in range(n):
+        t=A[i,1]
+        for j in range(n):
+            if i!=j:
+                t*=(a-A[j,0])/(A[i,0]-A[j,0])
+        s+=t
+    return s
+A=[[0.4,0.41075],[0.55,0.57815],[0.65,0.69675],[0.80,0.90],[0.95,1],[1.05,1.25382]]
+print(Larange_1(A,0.596))
